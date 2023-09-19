1ddd94ed17389573ad4d72423229b1b1a6956657
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-09-20 00:56:25 +0300

    0.5

--- netfilx/pages/auth.tsx
+++ netfilx/pages/auth.tsx
@@ -13,7 +13,17 @@ const Auth = () => {
     );
   }, []);
 
-  const register = useCallback(async () => {}, []);
+  const register = useCallback(async () => {
+    try {
+      await axios.post("/api/register", {
+        email,
+        name,
+        password,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover">
