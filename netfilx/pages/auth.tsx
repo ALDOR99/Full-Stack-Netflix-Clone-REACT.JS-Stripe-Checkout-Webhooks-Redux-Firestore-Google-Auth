4ae814381cee215ae4ab9743f4552f764611c203
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-09-19 23:38:38 +0300

    0.5

--- netfilx/pages/auth.tsx
+++ netfilx/pages/auth.tsx
@@ -11,6 +11,8 @@ const Auth = () => {
       currentVariant === "login" ? "register" : "login"
     );
   }, []);
+  const register = useCallback(async () => {});
+
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
