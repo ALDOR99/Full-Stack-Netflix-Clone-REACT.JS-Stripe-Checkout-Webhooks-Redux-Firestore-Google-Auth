75e37053fee489d91a4e462cbc17c9c19b15ac1b
Alis Durna <84575898+ALDOR99@users.noreply.github.com>
2023-09-27 00:51:09 +0300

    0.6

--- netfilx/pages/auth.tsx
+++ netfilx/pages/auth.tsx
@@ -1,7 +1,14 @@
 import axios from "axios";
 import { useCallback, useState } from "react";
 import Input from "@/components/Input";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub, FaGoogle } from "react-icons/fa";
+
 const Auth = () => {
+  const router = useRouter();
+
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
@@ -13,6 +20,21 @@ const Auth = () => {
     );
   }, []);
 
+  const login = useCallback(async () => {
+    try {
+      await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: "/",
+      });
+
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
+
   const register = useCallback(async () => {
     try {
       await axios.post("/api/register", {
@@ -20,10 +42,11 @@ const Auth = () => {
         name,
         password,
       });
+      login();
     } catch (error) {
       console.log(error);
     }
-  }, []);
+  }, [email, name, password, login]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover">
@@ -59,9 +82,47 @@ const Auth = () => {
                   value={password}
                 />
               </div>
-              <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+              <button
+                onClick={variant === "login" ? login : register}
+                className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+              >
                 {variant === "Login" ? "login " : "Sign up"}
               </button>
+
+              <div className="flex flex-row items-center gap-4 mt-8 justify-center ">
+                <div
+                  className="
+                    w-10 
+                    h-10 
+                    bg-white 
+                    rounded-full
+                    flex 
+                    items-center
+                    justify-center
+                    cursor-pointer
+                    hover:opacity-80
+                    transition"
+                >
+                  <FcGoogle size={30} />
+                </div>
+                <div
+                  onClick={() => signIn("github", { callbackUrl: "/" })}
+                  className="
+                    w-10 
+                    h-10 
+                    bg-white 
+                    rounded-full
+                    flex 
+                    items-center
+                    justify-center
+                    cursor-pointer
+                    hover:opacity-80
+                    transition"
+                >
+                  <FaGithub size={30} />
+                </div>
+              </div>
+
               <p className="text-neutral-500 mt-12 ">
                 {variant === "login"
                   ? "First time using Netfilx"
